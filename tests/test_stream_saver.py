4525d57d26dc614a6a10ef7756de3188e5ef286c
Genzo4 <genzo@bk.ru>
2022-06-24 09:29:09 +1000

    first tests

--- tests/test_stream_saver.py
+++ tests/test_stream_saver.py
@@ -2,5 +2,29 @@ import pytest
 from stream_saver_g4 import StreamSaver
 
 
-def test_1():
-    pass
+def test_defaults():
+    stream = StreamSaver()
+
+    assert stream.streamURL == ''
+    assert stream.outputTemplate == 'output_%Y-%m-%d_%H-%M-%S.ts'
+    assert stream.segmentTime == '01:00:00'
+
+
+def test_setVars_1():
+    stream = StreamSaver('rtsp://localhost:12345', '%H-%M-%S.ts', '00:10:00')
+
+    assert stream.streamURL == 'rtsp://localhost:12345'
+    assert stream.outputTemplate == '%H-%M-%S.ts'
+    assert stream.segmentTime == '00:10:00'
+
+
+def test_setVars_2():
+    stream = StreamSaver(outputTemplate='out.ts',
+                         segmentTime='12:34:56',
+                         streamURL='rtsp://user:pass@localhost:4321')
+
+    assert stream.streamURL == 'rtsp://user:pass@localhost:4321'
+    assert stream.outputTemplate == 'out.ts'
+    assert stream.segmentTime == '12:34:56'
+
+
